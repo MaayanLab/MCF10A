687ec30c24b175b9bd28b5a5a1c53694d77950fa
Katie Lin <kl2532@columbia.edu>
2016-08-18 12:19:23 -0400

    modified template Dockerfile

--- MCF10A_app/boot.sh
+++ MCF10A_app/boot.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 adduser --disabled-password --gecos '' r
-cd /flask-apache/
-mod_wsgi-express setup-server wsgi.py --port=80 --user r --group r --server-root=/etc/flask-apache --socket-timeout=600
-chmod a+x /etc/flask-apache/handler.wsgi
-/etc/flask-apache/apachectl start
-tail -f /etc/flask-apache/error_log
+cd /mcf10a/
+mod_wsgi-express setup-server wsgi.py --port=80 --user r --group r --server-root=/etc/mcf10a --socket-timeout=600
+chmod a+x /etc/mcf10a/handler.wsgi
+/etc/mcf10a/apachectl start
+tail -f /etc/mcf10a/error_log
