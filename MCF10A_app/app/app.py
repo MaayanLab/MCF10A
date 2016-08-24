e5c6dca12d3c33d17e7a852d0bf716aaeb2d95f9
Katie Lin <kl2532@columbia.edu>
2016-08-24 08:16:46 -0400

    starting drug pages

--- MCF10A_app/app/app.py
+++ MCF10A_app/app/app.py
@@ -13,17 +13,22 @@ app = Flask(__name__, static_url_path=ENTRY_POINT + 'static', static_folder='sta
 
 @app.route(ENTRY_POINT, methods=['GET'])
 @app.route(ENTRY_POINT + "index.html", methods=['GET'])
-@app.route(ENTRY_POINT + "assays.html", methods=['GET'])
+@app.route(ENTRY_POINT + "assays", methods=['GET'])
 def assays():
     return render_template("assays.html", menu_item="assays")
 
-@app.route(ENTRY_POINT + "drugs.html", methods=['GET'])
+@app.route(ENTRY_POINT + "drugs", methods=['GET'])
 def drugs():
     return render_template("drugs.html", menu_item="drugs")
 
-@app.route(ENTRY_POINT + "analysis.html", methods=['GET'])
+@app.route(ENTRY_POINT + "analysis", methods=['GET'])
 def analysis():
-    #json_data = get_my_json()
+    #json_data = get_my_json() 
     test_dict = {"a": 1, "b": 2, "c": 3}
     all_dicts = [test_dict, test_dict, test_dict]
     return render_template("analysis.html", json_data=all_dicts, menu_item="analysis")
+
+@app.route(ENTRY_POINT + "drugs/<drug>", methods=['GET'])
+def drug_view(drug):
+    print drug
+    return render_template("pages/drug-view.html", menu_item="drugs", drug_selected=drug)
